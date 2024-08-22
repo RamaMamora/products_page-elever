8d125ccfb51e8f9216a457460fd08bee3ea4beac
RamaMamora <rama.mamora@icloud.com>
2024-08-22 13:44:47 +0200

    update

--- js/templates.js
+++ js/templates.js
@@ -2,17 +2,25 @@
 
 export const productListTmpl = (product) =>
   `
-        <p><b>${product.title}</b></p>
+  <div class="container"
+        <h3><b>${product.title}</b></h3>
+        <img src="${product.image}">
         <p><i>${product.price} kr</i></p>
-        <img src="${product.image}${product.id}">
+        <article class="info">
     <button><a href="page.html?id=${product.id}">Se produkt</a></button> 
+    <button id=“${product.id}”>Hurtigvisning</button>
+    </article>
+    </div>
     `;
 
 export const productPageTmpl = (product) =>
   `
+  <div class="container"
         <h2>${product.title}</h2>
         <p><i>${product.price} kr</i></p>
         <p>${product.description}</p>
-        <img src="${product.image}${product.id}">
+        <img src="${product.image}">
     <button><a href="index.html">tilbage</a></button> 
+   
+    </div>
     `;
